311b1c78b0bd2bd547b346ae3b8d34f3fdd98db9
Megan Masanz <memasanz@microsoft.com>
2021-10-18 14:47:23 -0500

    updated eval and register

--- sourcecode/03_devopspipeline/pipeline_step_scripts/evaluate_and_register.py
+++ sourcecode/03_devopspipeline/pipeline_step_scripts/evaluate_and_register.py
@@ -10,6 +10,7 @@ import shutil
 parser = argparse.ArgumentParser("Evaluate model and register if more performant")
 parser.add_argument('--exp_trained_model_pipeline_data', type=str, required=True)
 
+
 args, _ = parser.parse_known_args()
 exp_trained_model_pipeline_data = args.exp_trained_model_pipeline_data
 
@@ -49,7 +50,9 @@ model_description = 'Diabetes model'
 model_list = Model.list(ws, name=model_name, latest=True)
 first_registration = len(model_list)==0
 
-updated_tags = {'AUC': current_model_AUC}
+build_id = os.getenv('BUILD_BUILDID', default='1')
+
+updated_tags = {'AUC': current_model_AUC, 'BUILD_ID' : build_id}
 
 print('updated tags')
 print(updated_tags)
@@ -75,7 +78,11 @@ else:
         #model = run.register_model(model_name, model_path='model_files', description=model_description, model_framework='Tensorflow/Keras', model_framework_version=tf.__version__, tags=updated_tags, datasets=formatted_datasets, sample_input_dataset = training_dataset)
         run.register_model(model_path=relative_model_path, model_name='diabetes_model',
                    tags=updated_tags,
-                   properties={'AUC': current_model_AUC, 'Accuracy': current_model_accuracy})
+                   properties={'AUC': current_model_AUC, 'Accuracy': current_model_accuracy, 'BUILD_ID' : build_id})
+                   
+ 
+        print('model version = ' + os.getenv("BUILD_ModelID", default=''))
+        
     else:
         print('New model does not perform better than existing model. Cancel run.')
         run.cancel()
