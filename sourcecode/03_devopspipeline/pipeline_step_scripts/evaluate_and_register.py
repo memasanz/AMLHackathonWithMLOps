3f29beb9a27be6066e3ccdd9614f66249e09297d
Megan Masanz <memasanz@microsoft.com>
2021-10-18 23:14:32 -0500

    update

--- sourcecode/03_devopspipeline/pipeline_step_scripts/evaluate_and_register.py
+++ sourcecode/03_devopspipeline/pipeline_step_scripts/evaluate_and_register.py
@@ -10,14 +10,14 @@ import shutil
 parser = argparse.ArgumentParser("Evaluate model and register if more performant")
 parser.add_argument('--exp_trained_model_pipeline_data', type=str, required=True)
 parser.add_argument('--model_name', type=string, required=True)
-parser.add_argument('--build_id', type=int, required=True)
+parser.add_argument('--build_number', type=string, required=True)
 
 
 
 args, _ = parser.parse_known_args()
 exp_trained_model_pipeline_data = args.exp_trained_model_pipeline_data
 model_name = args.model_name
-build_id = args.build_id
+build_number = args.build_number
 
 
 #Get current run
@@ -50,16 +50,17 @@ current_model_AUC = float(metrics['AUC'])
 current_model_accuracy = float(metrics['Accuracy'])
 
 # Get current model from workspace
-model_name = 'diabetes_model'
+#model_name = 'diabetes_model'
 model_description = 'Diabetes model'
 model_list = Model.list(ws, name=model_name, latest=True)
 first_registration = len(model_list)==0
 
-#build_id = os.getenv("BUILD_BUILDID", default='1')
+build_id = os.getenv("BUILD_BUILDID", default='1')
 
 print('build_id =' + build_id)
+print('build_number = ' + build_number)
 
-updated_tags = {'AUC': current_model_AUC, 'BUILD_ID' : build_id}
+updated_tags = {'AUC': current_model_AUC, 'build_number' : build_number}
 
 print('updated tags')
 print(updated_tags)
@@ -73,9 +74,9 @@ run.upload_folder(name=relative_model_path, path=exp_trained_model_pipeline_data
 if first_registration:
     print('First model registration.')
     #model = run.register_model(model_name, model_path='model_files', description=model_description, model_framework='sklearn', model_framework_version=tf.__version__, tags=updated_tags, datasets=formatted_datasets, sample_input_dataset = training_dataset)
-    run.register_model(model_path=relative_model_path, model_name='diabetes_model',
+    run.register_model(model_path=relative_model_path, model_name=model_name,
                    tags=updated_tags,
-                   properties={'AUC': current_model_AUC, 'BUILD_ID' : build_id})
+                   properties={'AUC': current_model_AUC, 'BUILD_ID' : build_id, 'BUILD_NUMBER' : build_number})
 else:
     #If a model has been registered previously, check to see if current model 
     #performs better. If so, register it.
@@ -85,10 +86,10 @@ else:
         #model = run.register_model(model_name, model_path='model_files', description=model_description, model_framework='Tensorflow/Keras', model_framework_version=tf.__version__, tags=updated_tags, datasets=formatted_datasets, sample_input_dataset = training_dataset)
         run.register_model(model_path=relative_model_path, model_name='diabetes_model',
                    tags=updated_tags,
-                   properties={'AUC': current_model_AUC, 'Accuracy': current_model_accuracy, 'BUILD_ID' : build_id})
+                   properties={'AUC': current_model_AUC, 'Accuracy': current_model_accuracy, 'BUILD_ID' : build_id, 'BUILD_NUMBER' : build_number})
                    
  
-        print('model version = ' + os.getenv("BUILD_ModelID", default=''))
+        
         
     else:
         print('New model does not perform better than existing model. Cancel run.')
